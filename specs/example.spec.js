dd79850567dce98df46d213fae3d4c155d442322
Dmitrii Bormotov <squier7@gmail.com>
2023-08-01 07:12:27 +0300

    Initial commit

---
+++ specs/example.spec.js
@@ -0,0 +1,12 @@
+describe('Operations with numbers', () => {
+  const a = 2
+  const b = 3
+
+  test('2 + 3 = 5', () => {
+    expect(a + b).toEqual(5)
+  })
+
+  test('2 - 3 = -1', () => {
+    expect(a - b).toEqual(-1)
+  })
+})
